2ff9f6594639471503ad9b0365f300fe1663a66f
Rho <rho@assume-wisely.com>
2022-04-30 06:55:13 -0600

    Implement gh-pages to publish on GitHub pages.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,15 @@
 module.exports = {
   siteMetadata: {
     title: `assume-wisely-blog`,
-    siteUrl: `https://www.yourdomain.tld`,
+    siteUrl: `https://rho-lall.github.io/assume-wisely-blog/`,
   },
   plugins: [],
+  {
+    "scripts": {
+      "deploy": "gatsby build --prefix-paths && gh-pages -d public"
+    }
+  }
+    module.exports = {
+      pathPrefix: "/assume-wisely-blog",
+  }
 }
