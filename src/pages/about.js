fa81e983f991143bae14bbec2efa883f458b5ed2
Rho <rho@assume-wisely.com>
2022-04-30 08:35:38 -0600

    Init about page.

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import * as React from 'react'
+
+const AboutPage = () => {
+    return (
+        <main>
+            <h1>test</h1>
+        </main>
+        
+    )
+
+}
+
+export default AboutPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,15 @@
 import * as React from 'react'
 
 const IndexPage = () => {
-return (
-  <main> 
-    <title>Assume Wisely Blog</title>
-    <h1>
-  </main>
+  return (
+    <main> 
+      <title>Assume Wisely Blog</title>
+      <h1>Welcome to my Gatsby Site</h1>
+      <p>I am following a tutorial.</p> 
+    
+    </main>
 )
 
 }
+
+export default IndexPage
