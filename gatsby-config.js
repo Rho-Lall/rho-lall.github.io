f2ce1efcd5c2513c10efee558f581f6f3dc8dff2
Rho <rho@assume-wisely.com>
2022-05-01 22:29:30 -0600

    Init blog logic.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,19 @@
 module.exports = {
   siteMetadata: {
-    title: `assume-wisely-blog`,
+    title: `Assume Wisely`,
     siteUrl: `https://rho-lall.github.io/assume-wisely-blog/`,
   },
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `thoughts`,
+        path: `${__dirname}/thoughts`,
+      }
+    },
+    "gatsby-plugin-mdx",
   ],
 
   pathPrefix: "/assume-wisely-blog",
