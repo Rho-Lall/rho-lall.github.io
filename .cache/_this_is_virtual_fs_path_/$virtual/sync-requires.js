e998ec73c83ed71d4a0d5f748b2df0f08b99b9ff
Rho <rho@assume-wisely.com>
2022-05-04 20:17:00 -0600

    Publish blog at rho-lall.github.io

---
+++ .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
@@ -0,0 +1,14 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("/Users/Rho.l/Documents/CODE/githubIO/Rho-Lall.github.io/.cache/dev-404-page.js")),
+  "component---src-pages-404-js": preferDefault(require("/Users/Rho.l/Documents/CODE/githubIO/Rho-Lall.github.io/src/pages/404.js")),
+  "component---src-pages-about-js": preferDefault(require("/Users/Rho.l/Documents/CODE/githubIO/Rho-Lall.github.io/src/pages/about.js")),
+  "component---src-pages-index-js": preferDefault(require("/Users/Rho.l/Documents/CODE/githubIO/Rho-Lall.github.io/src/pages/index.js")),
+  "component---src-pages-thoughts-index-js": preferDefault(require("/Users/Rho.l/Documents/CODE/githubIO/Rho-Lall.github.io/src/pages/thoughts/index.js")),
+  "component---src-pages-thoughts-mdx-slug-js": preferDefault(require("/Users/Rho.l/Documents/CODE/githubIO/Rho-Lall.github.io/src/pages/thoughts/{mdx.slug}.js"))
+}
+
