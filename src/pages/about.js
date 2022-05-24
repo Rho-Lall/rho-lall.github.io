2c74db35c98e73359c02657e3a215662dd83c809
Rho <rho@assume-wisely.com>
2022-05-23 23:28:15 -0600

    Add image to about me page.

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,5 @@
 
-//import { StaticImage } from 'gatsby-plugin-image'
+import { StaticImage } from 'gatsby-plugin-image'
 import * as React from 'react'
 //import {Link, graphql } from 'gatsby'
 //import { MDXRenderer } from 'gatsby-plugin-mdx'
@@ -17,7 +17,12 @@ const AboutPage = () => {
           <p className='mb-5'>I enjoy coding. I enjoyed data.</p>
           <p className='mb-5'>The passion that drives me is learning and uncovering new insight. </p>
           <p className='mb-5'>I dabble in architecture. I’m currently building a second-generation data stack that includes Looker and Snowflake. I’m playing with DBT. I've built databases,  APIs, websites . . . and I've set my sites on blockchain development specifically using XRP and XLM.</p>
-          <p className='mb-5'>I think I will document my learning path as a professional developer because that doesn't exist outside of the Ethereum community. There's a lot of attention and resources for Ethereum but frankly, XRP/XLM are better technologies. There will be demand for these technologies in the future. I'm hoping to do my part to contribute to the XRP XLM community.</p>
+          <p className='mb-10'>I think I will document my learning path as a professional developer because that doesn't exist outside of the Ethereum community. There's a lot of attention and resources for Ethereum but frankly, XRP/XLM are better technologies. There will be demand for these technologies in the future. I'm hoping to do my part to contribute to the XRP XLM community.</p>
+
+          <div className=''>
+          <StaticImage src="../images/about_rho_lall.png"/>
+          </div>
+
         </Layout> 
 
     )
