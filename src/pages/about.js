e4d55fd21c90c806d988b54f67d4038ef78e8e74
Rho <rho@assume-wisely.com>
2023-04-14 16:42:59 -0600

    Update about me.

--- src/pages/about.js
+++ src/pages/about.js
@@ -12,17 +12,16 @@ const AboutPage = () => {
         <Layout pageTitle={'Who is Rho Lall?'}>
             
           <h1 className='heading mb-5'>Who is Rho Lall?</h1>
-            
-          <p className='mb-5'>I am a full-stack, business intelligence developer.</p>
-          <p className='mb-5'>I enjoy coding. I enjoyed data.</p>
-          <p className='mb-5'>The passion that drives me is learning and uncovering new insight. </p>
-          <p className='mb-5'>I dabble in architecture. I’m currently building a second-generation data stack that includes Looker and Snowflake. I’m playing with DBT. I've built databases,  APIs, websites . . . and I've set my sites on blockchain development specifically using XRP and XLM.</p>
-          <p className='mb-10'>I think I will document my learning path as a professional developer because that doesn't exist outside of the Ethereum community. There's a lot of attention and resources for Ethereum but frankly, XRP/XLM are better technologies. There will be demand for these technologies in the future. I'm hoping to do my part to contribute to the XRP XLM community.</p>
-
+           
           <div className=''>
           <StaticImage src="../images/about_rho_lall.png"/>    
           </div>
-          
+ 
+          <p className='mb-5'>I am a full-stack, business intelligence developer.</p>
+          <p className='mb-5'>I enjoy coding. I enjoyed data.</p>
+          <p className='mb-5'>The passion that drives me is learning and uncovering new insight. </p>
+          <p className='mb-5'>I dabble in architecture. I’m currently building a second-generation data stack using DBT, Snowflake, Looker, and Streamlit. I've built databases,  APIs, websites . . . and I've set my sites on blockchain development specifically using XRP and XLM.</p>
+
         </Layout> 
 
     )
